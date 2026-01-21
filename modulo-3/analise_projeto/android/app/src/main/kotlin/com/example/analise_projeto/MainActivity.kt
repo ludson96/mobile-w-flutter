79d4f5aa4c84ddc0716ca56efed6321cef0ff3ef
ludson96 <ludson_ps27@hotmail.com>
2026-01-21 11:38:28 -0300

    feat: :tada: analisando novo projeto criado

---
+++ modulo-3/analise_projeto/android/app/src/main/kotlin/com/example/analise_projeto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.analise_projeto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- modulo-3/primeiro_aplicativo/lib/main.dart
+++ modulo-3/primeiro_aplicativo/lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(MaterialApp(home: HomePage()));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +24,23 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("Você clicou no botão:"), Text("10 vezes")],
+          children: [
+            Text("Você clicou no botão:", style: TextStyle(fontSize: 24)),
+            Text(
+              "$count vezes",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
       ),
     );
   }
