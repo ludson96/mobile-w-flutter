1cfe2101c5f94ccc3e4fb8f276e24fcb5b29811c
ludson96 <ludson_ps27@hotmail.com>
2026-01-27 18:20:58 -0300

    feat: :sparkles: creade text and facebook button

--- modulo-3/clone_home_medium/lib/pages/home_medium.page.dart
+++ modulo-3/clone_home_medium/lib/pages/home_medium.page.dart
@@ -28,6 +28,44 @@ class HomeMediumPage extends StatelessWidget {
               SocialButton(image: "assets/images/google.png", social: "Google"),
               SizedBox(height: 15),
               SocialButton(image: "assets/images/email.png", social: "Email"),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.black)),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+                    child: Text(
+                      "Or, sign up with",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  Expanded(child: Divider(color: Colors.black)),
+                ],
+              ),
+              SizedBox(height: 30),
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset("assets/images/facebook.png", height: 35),
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: "Already have an account?",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                    children: [
+                      TextSpan(
+                        text: " Sign in",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
