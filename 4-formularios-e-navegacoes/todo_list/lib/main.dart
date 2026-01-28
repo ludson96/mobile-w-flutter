ab7831a7d3ee867335d09b9a4e6b34f3fda4c961
ludson96 <ludson_ps27@hotmail.com>
2026-01-28 17:15:59 -0300

    feat: :bug: fix bug

---
+++ 4-formularios-e-navegacoes/todo_list/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/pages/tasks_list.page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo list',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      ),
+      home: const TasksListPage(),
+    );
+  }
+}
---
+++ 4-formularios-e-navegacoes/todo_list/lib/pages/tasks_list.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TasksListPage extends StatelessWidget {
+  const TasksListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo List')),
+    );
+  }
+}
