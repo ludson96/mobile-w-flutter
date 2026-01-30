7dce93b0ac6f6c4227c09363e1aafd3ac71130ee
ludson96 <ludson_ps27@hotmail.com>
2026-01-30 15:20:46 -0300

    feat: :sparkles: create first card for todo

--- 4-formularios-e-navegacoes/todo_list/lib/pages/tasks_list.page.dart
+++ 4-formularios-e-navegacoes/todo_list/lib/pages/tasks_list.page.dart
@@ -10,6 +10,31 @@ class TasksListPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
         label: const Text("Nova tarefa"),
+        // icon: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Card(
+              elevation: 3,
+              color: Colors.indigo[100],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                title: const Text("Marcar uma reunião"),
+                subtitle: const Text("Reunião sobre negócios"),
+                leading: Checkbox(value: true, onChanged: (value) {}),
+                trailing: const Icon(Icons.star, color: Colors.indigo),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
