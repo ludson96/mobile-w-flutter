f57fd78e321ae7af01de32906ad0fb9e8df0869c
ludson96 <ludson_ps27@hotmail.com>
2026-02-19 15:23:45 -0300

    feat: :sparkles: add newcolor and structure for cards pokemon

--- 6-animacoes-e-integracao-w-api/pokedex/lib/pages/home.page.dart
+++ 6-animacoes-e-integracao-w-api/pokedex/lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,8 +8,54 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(children: [Align(alignment: AlignmentGeometry.topLeft,
-        child: Text("Pokedex", style: TextStyle(),),)]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Pokédex",
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Proure um pokemon pelo nome ou pelo identificador",
+                style: TextStyle(color: primaryColor, fontSize: 18),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Nome ou identificador",
+                  hintStyle: const TextStyle(color: primaryColor),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.search, color: primaryColor),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Card(color: Colors.green[200]);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
