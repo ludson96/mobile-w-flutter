142d316d052532b3172f2df34a17c168c0a95fb1
ludson96 <ludson_ps27@hotmail.com>
2026-05-08 14:54:53 -0300

    feat: :sparkles: add contention of errors and style maps

--- 9-google-maps-e-localizacao/clone_maps_ios/lib/pages/home/map_sample.page.dart
+++ 9-google-maps-e-localizacao/clone_maps_ios/lib/pages/home/map_sample.page.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:clone_maps_ios/pages/home/widget/error_settings_map.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapSample extends StatefulWidget {
   const MapSample({super.key});
@@ -14,14 +16,9 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> with WidgetsBindingObserver {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  late String mapStyle;
 
-  CameraPosition? _initialPosition;
-
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<void> _determinePosition() async {
+  Future<CameraPosition> _determinePosition() async {
     // Test if location services are enabled.
     final serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
@@ -31,38 +28,29 @@ class MapSampleState extends State<MapSample> with WidgetsBindingObserver {
       return Future.error('SERVICE_NOT_ENABLE');
     }
 
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('PERMISSION_LOCATION_DENIED');
-      }
-    }
+    final permission = await Geolocator.checkPermission();
 
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
+    if (permission == LocationPermission.denied) {
       return Future.error('PERMISSION_LOCATION_DENIED');
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
     final currentPosition = await Geolocator.getCurrentPosition();
 
-    _initialPosition = CameraPosition(
+    final cameraPosition = CameraPosition(
       target: LatLng(currentPosition.latitude, currentPosition.longitude),
       zoom: 14,
     );
+
+    return cameraPosition;
   }
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
+    rootBundle
+        .loadString('assets/map/style.json')
+        .then((style) => mapStyle = style);
   }
 
   @override
@@ -94,38 +82,38 @@ class MapSampleState extends State<MapSample> with WidgetsBindingObserver {
             final error = snapshot.error as String;
 
             if (error == 'SERVICE_NOT_ENABLE') {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        "O serviço de localização está desabilitado. Você  precisa habilitar para utilizá-lo",
-                        textAlign: .center,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 20),
-                      OutlinedButton(
-                        onPressed: () async {
-                          await Geolocator.openLocationSettings();
-                        },
-                        child: Text("Habilitar localização"),
-                      ),
-                    ],
-                  ),
-                ),
+              return ErrorSettingsMap(
+                textButton: "Habilitar localização",
+                textError:
+                    "O serviço de localização está desabilitado. Você  precisa habilitar para utilizá-lo",
+                onPressed: () async {
+                  await Geolocator.openLocationSettings();
+                },
               );
             }
 
-            if (error == 'PERMISSION_LOCATION_DENIED') {}
+            if (error == 'PERMISSION_LOCATION_DENIED') {
+              return ErrorSettingsMap(
+                textButton: "Conceder permissão de localização",
+                textError:
+                    "O aplicativo precisa de permissão de localização para funcionar.",
+                onPressed: () async {
+                  final permission = await Geolocator.requestPermission();
+                  if (permission == LocationPermission.deniedForever) {
+                    await Geolocator.openAppSettings();
+                  }
+                },
+              );
+            }
           }
 
           return GoogleMap(
             mapType: MapType.normal,
-            initialCameraPosition: _initialPosition!,
+            initialCameraPosition: snapshot.data!,
             myLocationButtonEnabled: false,
             myLocationEnabled: true,
             zoomControlsEnabled: false,
+            style: mapStyle,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
---
+++ 9-google-maps-e-localizacao/clone_maps_ios/lib/pages/home/widget/error_settings_map.widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ErrorSettingsMap extends StatelessWidget {
+  final String textError;
+  final String textButton;
+  final void Function() onPressed;
+
+  const ErrorSettingsMap({
+    super.key,
+    required this.textError,
+    required this.textButton,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Center(
+        child: Column(
+          children: [
+            Text(textError, textAlign: .center, style: TextStyle(fontSize: 16)),
+            SizedBox(height: 20),
+            OutlinedButton(onPressed: onPressed, child: Text(textButton)),
+          ],
+        ),
+      ),
+    );
+  }
+}
